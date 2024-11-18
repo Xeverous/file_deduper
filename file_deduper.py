9abb85ec4e6784ac3cc58dd8ce4cde633c76315c
Xeverous <20820409+Xeverous@users.noreply.github.com>
2024-11-18 22:02:21 +0100

    improved formatting and added more statistics

--- file_deduper.py
+++ file_deduper.py
@@ -21,6 +21,22 @@ def to_hex(checksum: Optional[bytes]) -> str:
     else:
         return checksum.hex()
 
+# modified https://stackoverflow.com/a/1094933/4818802
+# - removed suffix parameter
+# - added space before unit
+# - added a condition to prevent "1.0 B"
+# - removed leading spaces in formatting
+# - added type hints
+def pretty_byte_size(num: int) -> str:
+    for unit in ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"):
+        if abs(num) < 1024.0:
+            if unit == "B":
+                return f"{num} {unit}"
+            else:
+                return f"{num:.1f} {unit}"
+        num /= 1024.0
+    return f"{num:.1f} YiB"
+
 class Entry:
     def __init__(self, parent: "Optional[Entry]",
                  path: Union[str, bytes, os.PathLike],
@@ -42,7 +58,7 @@ class Entry:
         size = "{:>12}".format(self.size)
         # removing .strftime() brings default formatting (which also prints second fractions)
         date = datetime.datetime.fromtimestamp(self.date_modified).strftime('%Y-%m-%d %H:%M:%S')
-        return f"{date} {size} {to_hex(self.checksum)} {self.path}"
+        return f"{date} {size} {self.path}"
 
 def make_entry(parent: Optional[Entry],
                path: Union[str, bytes, os.PathLike],
@@ -58,10 +74,11 @@ def make_entry(parent: Optional[Entry],
 
 class Database:
     def __init__(self):
-        self.entries_by_name:     dict[str, list[Entry]] = {}
+        self.entries_by_name:     dict[str,   list[Entry]] = {}
         self.entries_by_checksum: dict[bytes, list[Entry]] = {}
-        self.total_dirs = 0
+        self.total_dirs  = 0
         self.total_files = 0
+        self.total_size  = 0
 
     def _update_dict(self, d: dict, key, entry: Entry):
         l = d.get(key)
@@ -79,24 +96,37 @@ class Database:
             # directories should compare checksums of their children
             self._update_dict(self.entries_by_checksum, entry.checksum, entry)
             self.total_files += 1
+            self.total_size += entry.size
 
-    def print(self, all: bool = False):
+    def print(self):
         print("duplicate by name:")
         for name, entries in self.entries_by_name.items():
-            if all or len(entries) > 1:
-                print() # empty line to separate groups of duplicates
+            if len(entries) > 1:
+                print(f"\n{name}") # empty line to separate groups of duplicates
                 for entry in entries:
                     print(entry)
 
+        space_taken_by_duplicates = 0
+        duplicate_sets = 0
         print("\n\nduplicate by checksum:")
         for checksum, entries in self.entries_by_checksum.items():
-            if all or len(entries) > 1:
-                print() # empty line to separate groups of duplicates
+            if len(entries) > 1:
+                print(f"\n{to_hex(checksum)}") # empty line to separate groups of duplicates
                 for entry in entries:
                     print(entry)
+                    space_taken_by_duplicates += entry.size
+                # It can be very safely assumed that the size of each file with same hash is identical.
+                # As of writing this, there is no known SHA-256 collision.
+                # -1 because one copy should remain.
+                space_taken_by_duplicates += entries[0].size * (len(entries) - 1)
+                duplicate_sets += 1
 
         print(f"\n\ntotal dirs: {self.total_dirs}")
         print(f"total files: {self.total_files}")
+        print(f"total size: {pretty_byte_size(self.total_size)}")
+        print()
+        print(f"duplicate sets: {duplicate_sets}")
+        print(f"space taken by duplicates: {pretty_byte_size(space_taken_by_duplicates)}")
 
 
 def walktree(parent: Entry, database: Database):
@@ -127,9 +157,6 @@ if __name__ == "__main__":
         exit(1)
     scan(sys.argv[1:])
 
-# dict by size+checksum, not checksum
-# print total size of duplicates
-# print total amount of duplicate sets
 # remove files that have "XYZ" and "XYZ (1)" names
 # printing duplicates - print by discovery order, path (might be the same) or by date
 # prompt for removal if in the same directory?
