ff085f95a9e3e1035f9505aeb5dd35787a828ce0
Xeverous <20820409+Xeverous@users.noreply.github.com>
2024-11-18 17:31:16 +0100

    more type hints and some simplifications

--- file_deduper.py
+++ file_deduper.py
@@ -4,7 +4,7 @@ import datetime
 import sys
 import os
 import stat
-from typing import List, Optional, Union
+from typing import AnyStr, List, Optional, Union
 from hashlib import sha256
 
 # https://stackoverflow.com/questions/53418046/how-do-i-type-hint-a-variable-that-can-be-passed-as-the-first-argument-to-open
@@ -24,28 +24,28 @@ def to_hex(checksum: Optional[bytes]) -> str:
 class Entry:
     def __init__(self, parent: "Optional[Entry]",
                  path: Union[str, bytes, os.PathLike],
-                 name: Union[str, bytes, os.PathLike],
                  is_dir: bool, date_modified: int,
                  size: int, checksum: Optional[bytes]):
         self.parent = parent
         self.children: list[Entry] = []
         self.path = path
-        self.name = name
         self.is_dir = is_dir
         self.date_modified = date_modified
         self.size = size
         self.checksum = checksum
 
-    def __repr__(self):
+    def name(self) -> AnyStr:
+        return os.path.basename(self.path)
+
+    def __repr__(self) -> str:
         # 12 is enough for ~9.1 TiB
         size = "{:>12}".format(self.size)
         # removing .strftime() brings default formatting (which also prints second fractions)
         date = datetime.datetime.fromtimestamp(self.date_modified).strftime('%Y-%m-%d %H:%M:%S')
         return f"{date} {size} {to_hex(self.checksum)} {self.path}"
 
-def make_entry(parent: Entry,
+def make_entry(parent: Optional[Entry],
                path: Union[str, bytes, os.PathLike],
-               name: Union[str, bytes, os.PathLike],
                stats: os.stat_result) -> Entry:
     is_dir = stat.S_ISDIR(stats.st_mode)
     if is_dir:
@@ -54,7 +54,7 @@ def make_entry(parent: Entry,
     else:
         checksum = sha256sum(path)
         size = stats.st_size
-    return Entry(parent, path, name, is_dir, stats.st_mtime, size, checksum)
+    return Entry(parent, path, is_dir, stats.st_mtime, size, checksum)
 
 class Database:
     def __init__(self):
@@ -70,9 +70,8 @@ class Database:
         else:
             d.update([(key, [entry])])
 
-
     def add(self, entry: Entry):
-        self._update_dict(self.entries_by_name, entry.name, entry)
+        self._update_dict(self.entries_by_name, entry.name(), entry)
         if entry.is_dir:
             self.total_dirs += 1
         else:
@@ -100,16 +99,16 @@ class Database:
         print(f"total files: {self.total_files}")
 
 
-def walktree(root: Union[str, bytes, os.PathLike], parent: Entry, database: Database):
-    for name in os.listdir(root):
-        path = os.path.join(root, name)
+def walktree(parent: Entry, database: Database):
+    for name in os.listdir(parent.path):
+        path = os.path.join(parent.path, name)
         stats = os.lstat(path)
         if stat.S_ISDIR(stats.st_mode) or stat.S_ISREG(stats.st_mode):
-            entry = make_entry(parent, path, name, stats)
+            entry = make_entry(parent, path, stats)
             parent.children.append(entry)
             database.add(entry)
             if entry.is_dir:
-                walktree(path, entry, database)
+                walktree(entry, database)
                 for child in entry.children:
                     entry.size += child.size
         else:
@@ -118,8 +117,8 @@ def walktree(root: Union[str, bytes, os.PathLike], parent: Entry, database: Data
 def scan(root_dirs: List[Union[str, bytes, os.PathLike]]):
     database = Database()
     for root_dir in root_dirs:
-        root_entry = make_entry(None, root_dir, os.path.basename(root_dir), os.lstat(root_dir))
-        walktree(root_dir, root_entry, database)
+        root_entry = make_entry(None, root_dir, os.lstat(root_dir))
+        walktree(root_entry, database)
     database.print()
 
 if __name__ == "__main__":
@@ -135,6 +134,3 @@ if __name__ == "__main__":
 # printing duplicates - print by discovery order, path (might be the same) or by date
 # prompt for removal if in the same directory?
 # scanning - by name or by cheksum - make it an option
-
-# deduper
-# depurator, rarefy-ier?, reducer, shaver, moderator
